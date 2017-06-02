02d94f16f6e4486c883011dda79d3e3f382ca076
Danny Suarez <dsuarezlogans@gmail.com>
2017-06-02 16:00:13 -0400

    subiendo app de formulario solicitd demo financiera efectiva

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || '3030';
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile('public/index.html');
+});
+
+app.listen(port, function () {
+  console.log(`Server Starts on ${port}`);
+});
